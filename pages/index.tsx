1e8cd2d8f6e0cb6fd6c5e8f596053a7908ca7d7b
shashwat <shashwat0212@gmail.com>
2022-01-14 18:02:40 +0530

    sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Spotify 2.0</title>
-      </Head>
-
+    <div className="">
       <h1>Spotify</h1>
+      <div>
+        {/* Sidebar */}
+        {/* Center */}
+      </div>
+      <div>
+        {/* player */}
+        </div>
     </div>
   )
 }
